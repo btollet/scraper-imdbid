264d5228d465cca84dc80b8dfdb080b05bbb82de
Benjamin <benjamin@MacBook-Pro-de-Benjamin.local>
2017-09-21 10:49:51 +0200

    search upgrade + bug correct

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ const cheerio = require('cheerio')
 const app = express()
 
 app.get('/:name', function (req, res) {
-  request(`http://www.imdb.com/find?ref_=nv_sr_fn&q=${req.params.name}&s=tt`, function(error, response, html){
+  request(`http://www.imdb.com/find?q=${req.params.name}&s=tt&ttype=ft&ref_=fn_ft`, function(error, response, html){
         if(!error){
             let regex = /(^\/title\/+|\/\?[^]+$)/mg
             let $ = cheerio.load(html)
             let link = $('#main .result_text a').first().attr('href')
             let title = $('#main .result_text a').first().text()
 
-            let imdbid = link.replace(regex, '')
-            res.send(`Titre: ${title} </br>id: ${imdbid}`)
+            if (link) {
+              let imdbid = link.replace(regex, '')
+              res.send(`Titre: ${title} </br>id: ${imdbid}`)
+            }
+            else {
+              res.send('Not found')
+            }
 
         }
     })
